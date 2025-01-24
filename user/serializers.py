ca819bbdfbbe68ce9b162a35624c7d19921bb827
KGZBekz1 <a4580880@gmail.com>
2025-01-24 19:18:56 +0600

    first commit

--- user/serializers.py
+++ user/serializers.py
@@ -1,29 +1,51 @@
-from .models import SMSCode
-from django.contrib.auth.models import User
 from rest_framework import serializers
-
-class RegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=100)
-    email = serializers.EmailField()
-    password = serializers.CharField(max_length=100)
-    confirm_password = serializers.CharField(max_length=100)
-
-    def validate(self, data):
-        if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError('Passwords are not the same')
-        return data
-
-    def validate_username(self, username):
-        try:
-            User.objects.get(username=username)
-        except User.DoesNotExist:
-            return username
-        raise serializers.ValidationError('Такой username уже занят')
-
-class loginSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=100)
-    password = serializers.CharField(max_length=100)
+from .models import Movie, Review, Director
 
 
-class SMSCodeSerializer(serializers.Serializer):
-    sms_code = serializers.CharField(max_length=100)
+class DirectorSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=100)
+    movie_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Director
+        fields = ('name', 'movie_count')
+
+    def get_movie_count(self, director):
+        return director.movies.count()
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ('id', 'text', 'movie', 'stars')
+
+
+class ReviewValiditySerializer(serializers.Serializer):
+    text = serializers.CharField()
+    movie = serializers.IntegerField()
+    stars = serializers.IntegerField(min_value=1, max_value=5)
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    director = DirectorSerializer()
+    reviews = ReviewSerializer(many=True)
+    average_rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Movie
+        fields = ('title', 'description', 'duration', 'director', 'reviews', 'average_rating')
+
+    def get_average_rating(self, movie):
+        reviews = movie.reviews.all()
+        if reviews:
+            sum_reviews = sum([review.stars for review in reviews])  # 6
+            average = sum_reviews / len(reviews)  # 6/2 = 3
+            return average
+        return None
+
+
+class MovieValidirySerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    duration = serializers.IntegerField()
+    director = serializers.IntegerField()
