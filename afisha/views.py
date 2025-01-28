4efb43395b28d4791fc5cb9ce11ddbc99e98766d
KGZBekz1 <a4580880@gmail.com>
2025-01-28 15:43:07 +0600

    views.py
    извините прошу но так пришлось

--- afisha/views.py
+++ afisha/views.py
@@ -1,70 +1,85 @@
-from itertools import product
-from wsgiref.validate import validator
-from django.shortcuts import render, get_object_or_404
-from rest_framework import status
-from rest_framework.decorators import api_view
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Movie, Review, Director
+from .serializers import MovieSerializer, ReviewSerializer, DirectorSerializer, MovieValidirySerializer, ReviewValiditySerializer
 from rest_framework.response import Response
-from .models import Movie, Director, Review
-from .serializers import MovieSerializer, DirectorSerializer, ReviewSerializer, MovieValiditySerializer
+from rest_framework import status
 
 
-@api_view(http_method_names=['GET'])
-def movie_list_view(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+class DirectorListAPIView(generics.ListCreateAPIView):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
 
 
-@api_view(http_method_names=['GET'])
-def movie_detail_view(request, id):
-    movie = get_object_or_404(Movie, id=id)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+class DirectorDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
+    lookup_field = 'id'
 
 
-def post(self, request, *args, **kwargs):
-    validator = MovieValiditySerializer(data=request.data)
-    if not validator.is_valid():
-        return Response(validator.errors, status=status.HTTP_400_BAD_REQUEST)
-    title = validator.validated_data('title')
-    description = validator.validated_data('description')
-    duration = validator.validated_data('duration')
-    director = validator.validated_data('director')
-    movie = Movie.objects.create(title=title, description=description, duration=duration, director=director)
-    movie.save()
-    return Response({'id': movie.id}, status=status.HTTP_201_CREATED)
-
-    def put(self, request, *args, **kwargs):
-        product_detail = self.get_object()
-        validator = MovieValiditySerializer(data=request.data)
-        if not validator.is_valid():
-            return Response(validator.errors, status=status.HTTP_400_BAD_REQUEST)
-        product_detail.title = validator.validated_data('title')
-        product_detail.description = validator.validated_data('description')
-        product_detail.duration = validator.validated_data('duration')
-        product_detail.director = validator.validated_data('director')
-        product_detail.save()
-        return Response(product_detail).data, status.HTTP_200_OK
+class MovieListAPIView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
 
     def post(self, request, *args, **kwargs):
-        validator = MovieValiditySerializer(data=request.data)
+        validator = MovieValidirySerializer(data=request.data)
         if not validator.is_valid():
             return Response(validator.errors, status=status.HTTP_400_BAD_REQUEST)
-        text = validator.validated_data('text')
-        movie = validator.validated_data('movie')
-        stars = validator.validated_data('stars')
-        review = validator.validated_data('review')
+        title = validator.validated_data['title']
+        description = validator.validated_data['description']
+        duration = validator.validated_data['duration']
+        director_id = validator.validated_data['director']
+        movie = Movie.objects.create(title=title, description=description, duration=duration, director_id=director_id)
+        movie.save()
+        return Response(MovieSerializer(movie).data, status=status.HTTP_201_CREATED)
+            
+
+class MovieDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    lookup_field = 'id'
+
+    def put(self, request, *args, **kwargs):
+        movie_detail = self.get_object()
+        validator = MovieValidirySerializer(data=request.data)
+        if not validator.is_valid():
+            return Response(validator.errors, status=status.HTTP_400_BAD_REQUEST)
+        movie_detail.title = validator.validated_data['title']
+        movie_detail.description = validator.validated_data['description']
+        movie_detail.duration = validator.validated_data['duration']
+        movie_detail.director_id = validator.validated_data['director']
+        movie_detail.save()
+        return Response(MovieSerializer(movie_detail).data, status=status.HTTP_200_OK)
+
+
+class ReviewListAPIView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def post(self, request, *args, **kwargs):
+        validator = ReviewValiditySerializer(data=request.data)
+        if not validator.is_valid():
+            return Response(validator.errors, status=status.HTTP_400_BAD_REQUEST)
+        text = validator.validated_data['text']
+        movie_id = validator.validated_data['movie']
+        stars = validator.validated_data['stars']
+        review = Review.objects.create(text=text, movie_id=movie_id, stars=stars)
         review.save()
         return Response(ReviewSerializer(review).data, status=status.HTTP_201_CREATED)
 
 
+class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    lookup_field = 'id'
+
     def put(self, request, *args, **kwargs):
         review_detail = self.get_object()
-        validator = MovieValiditySerializer(data=request.data)
+        validator = ReviewValiditySerializer(data=request.data)
         if not validator.is_valid():
             return Response(validator.errors, status=status.HTTP_400_BAD_REQUEST)
-        review_detail.text = validator.validated_data('text')
-        review_detail.movie = validator.validated_data('movie')
-        review_detail.stars = validator.validated_data('stars')
+        review_detail.text = validator.validated_data['text']
+        review_detail.movie_id = validator.validated_data['movie']
+        review_detail.stars = validator.validated_data['stars']
         review_detail.save()
         return Response(ReviewSerializer(review_detail).data, status=status.HTTP_200_OK)
